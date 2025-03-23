05d5ad9708d41b056963106b5dede2c55e9ea75d
niki0505 <bataclanknicole2004@gmail.com>
2025-03-23 17:49:53 +0800

    As

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,43 +4,43 @@ import Resident from "../src/models/Residents.js";
 
 const router = express.Router();
 
-router.post("/checkresident", async (req, res) => {
-  try {
-    const { firstname, lastname } = req.body;
+// router.post("/checkresident", async (req, res) => {
+//   try {
+//     const { firstname, lastname } = req.body;
 
-    const resident = await Resident.findOne({ firstname, lastname });
+//     const resident = await Resident.findOne({ firstname, lastname });
 
-    if (resident) {
-      return res.json({ exists: true });
-    } else {
-      return res.json({ exists: false });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error });
-  }
-});
+//     if (resident) {
+//       return res.json({ exists: true });
+//     } else {
+//       return res.json({ exists: false });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: "Server error", error });
+//   }
+// });
 
 router.post("/register", async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { firstname, lastname, username, password } = req.body;
     console.log("Received request body:", req.body);
-    if (!username || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+
+    const resident = await Resident.findOne({ firstname, lastname });
+
+    if (!resident) {
+      return res.json({ exists: false });
     }
 
     const user = new User({
       username,
       password,
+      resID: resident.resID,
     });
 
     await user.save();
 
     res.status(201).json({
       message: "User registered successfully. Please log in.",
-      user: {
-        username: user.username,
-        password: user.password,
-      },
     });
   } catch (error) {
     console.log("Error in register route", error);
