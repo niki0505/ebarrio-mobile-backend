9873bc5ed0297ee3a5ecce846035b362a32dda57
niki0505 <bataclanknicole2004@gmail.com>
2025-06-03 23:17:12 +0800

    Revisions

--- models/Certificates.js
+++ models/Certificates.js
@@ -1,5 +1,15 @@
 import mongoose from "mongoose";
 
+const CertificateCounterSchema = new mongoose.Schema({
+  _id: String,
+  seq: Number,
+});
+
+const CertificateCounter = mongoose.model(
+  "CertificateCounter",
+  CertificateCounterSchema
+);
+
 const certSchema = new mongoose.Schema(
   {
     certID: {
@@ -59,6 +69,37 @@ const certSchema = new mongoose.Schema(
   },
   { versionKey: false, timestamps: true }
 );
+
+async function assignCertNo(doc) {
+  if (
+    (!doc.certno && doc.status === "Not Yet Collected") ||
+    doc.status === "Collected"
+  ) {
+    const counter = await CertificateCounter.findByIdAndUpdate(
+      { _id: "certno" },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+    doc.certno = counter.seq;
+  }
+}
+
+// Pre-save hook
+certSchema.pre("save", async function (next) {
+  try {
+    if (this.isNew) {
+      await assignCertNo(this);
+    } else if (
+      (this.isModified("status") && this.status === "Not Yet Collected") ||
+      (this.status === "Collected" && !this.certno)
+    ) {
+      await assignCertNo(this);
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 const Certificate = mongoose.model("Certificate", certSchema);
 
